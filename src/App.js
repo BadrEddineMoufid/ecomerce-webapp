33f8c9178329e79e55a53194ab43c02cb1b4782c
unknown <bin.benh@gmail.com>
2019-11-02 18:19:57 +0100

    added redux and implemented user-reducer and root-reducer to Header and App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import {auth, createUserProfileDoc} from './components/firebase/firebase';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,44 +9,47 @@ import HomePage from './pages/homepage/homepage'
 import ShopPage from './pages/shoppage/shop';
 import Header from './components/header/header';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-
+import { setCurrentUser } from './redux/user/user-actions';
 
 
 class App extends React.Component {
   
-  constructor(){
-    super();
+  // constructor(){
+  //   super();
 
-    this.state={
-      currentUser: null
-    }
-  }
+  //   this.state={
+  //     currentUser: null
+  //   }
+  // }
 
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       
       if(userAuth){
         const userRef = await createUserProfileDoc(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          //console.log(snapShot.data());
-
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          //console.log(snapShot.data()); 
+          
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
 
           
         })
       }
-      else{
-        this.setState({currentUser: userAuth}, ()=> console.log(this.state));
-        //console.log(this.state);
-      }
+
+      setCurrentUser(userAuth);
+
+      // else{
+      //   this.setState({currentUser: userAuth}, ()=> console.log(this.state));
+      //   //console.log(this.state);
+      // }
       
       //console.log(this.state);
       //createUserProfileDoc(user);
@@ -62,7 +66,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
@@ -74,4 +78,10 @@ class App extends React.Component {
   
 }
 
-export default App;
+//dispatching actions to reducers
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,31 +2,40 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import {auth} from '../firebase/firebase';
+import { connect } from 'react-redux'; 
 
 import './header.scss';
 
 
-export default function Header({currentUser}) {
-    return (
-        <div className='header'>
-            <Link className='logo-container' to="/">
-                <Logo className='logo'/>
+const Header = ({currentUser}) => ( 
+    <div className='header'>
+
+        <Link className='logo-container' to="/">
+            <Logo className='logo'/>
+        </Link>
+
+        <div className='options'>
+
+            <Link className='option' to='/shop'>
+                SHOP
             </Link>
-            <div className='options'>
-                <Link className='option' to='/shop'>
-                    SHOP
-                </Link>
-                <Link className='option' to='/cantact'>
-                    CANTACT
-                </Link>
-                {
-                    currentUser ?
-                    <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
-                    :
-                    <Link className='option' to='/signin'>SIGN IN </Link>
-                }
-                
-            </div>
+
+            <Link className='option' to='/cantact'>
+                CANTACT
+            </Link>
+            {
+                currentUser ?
+                <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
+                :
+                <Link className='option' to='/signin'>SIGN IN </Link>
+            }
+            
         </div>
-    )
-}
+    </div>  
+);
+
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
